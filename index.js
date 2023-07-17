081d897f924d8934d26b28763d0f90b6cff00ba1
Anmol <anmol.volt@gmail.com>
2023-07-18 00:59:59 +0530

    fixed

--- index.js
+++ index.js
@@ -1,42 +1,10 @@
-import http from "http"
-import { pokemon,chimchar,bulbasaur } from "./package.js"
-import {lovePercent} from "./lovePercent.js"
-import fs from 'fs'
-console.log(lovePercent())
-console.log(pokemon,chimchar,bulbasaur)
+import express from "express";
+const app = express()
 
-
-
-
-
-
-
-const server = http.createServer((req,res)=>{
-    if(req.url==='/about'){
-        res.end("about page")
-    }
-    else if(req.url==='/contact'){
-        res.end("contact page")
-    }
-    else if(req.url==='/'){
-        
-            fs.readFile('index.html',(err,data)=>{
-                res.end(data)
-            })
-  
-    }
-    else if (req.url==='/love'){
-        res.end(`love percentage is ${lovePercent()}%`)
-    }
-    else if(req.url==='/info'){
-        res.end("info page")
-    }
-    else{
-        res.end("no page found")
-    }
+app.get("/",(req,res)=>{
+    res.status(201).render("index.ejs",{name:"anmol",age:18})
 })
 
- 
-server.listen(5000,()=>{
-    console.log("server is up on port 5000")
+app.listen(5000,()=>{
+    console.log("server running on port 5000")
 })
